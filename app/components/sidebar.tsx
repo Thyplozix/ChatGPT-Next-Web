6e334d64dffd82b0d3ca980c94d6aba57199826c
Adrienne Bienapfl <adrienne.bienapfl@fortifiedhq.com>
2023-05-27 04:23:03 -0600

    branding changes

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Adri GPT</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          For the sake of learning and memes.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
